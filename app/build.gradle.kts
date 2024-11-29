5b752ef67b03c5f71580d9e4b98c66c3c7b363e7
SousaLuan <153736882+SousaLuan@users.noreply.github.com>
2024-11-29 19:24:50 -0300

    Opa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.material:material:1.3.0")
+
 }
